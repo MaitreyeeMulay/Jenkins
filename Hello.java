48bd5e39e7cee163997b494aa7d953db2433cbda
MaitreyeeMulay <97637439+MaitreyeeMulay@users.noreply.github.com>
2024-10-18 15:41:56 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
-public class HelloWorld {
+public class Hello {
     public static void main(String[] args) {
-        // Print Hello, World! to the console
         System.out.println("Hello, World!");
     }
 }
